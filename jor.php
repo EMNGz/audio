40f40350bd9aa62338fb8d065b5d39c70fa5acc5
EMNGz <gameball326@gmail.com>
2024-03-01 09:32:07 +0700

    first commit

---
+++ jor.php
@@ -0,0 +1,118 @@
+<body>
+  <style type="text/css">
+<!--
+body {
+            background-color: #000000;
+        }
+        #ewt_main_structure_body a {
+            color: #ffee00 !important;
+        }
+-->
+</style>
+   <table cellspacing="5" cellpadding="5" width="990" align="center" border="0">
+     <tbody>
+       <tr>
+         <td colspan="4"></td>
+      </tr>
+       <tr>
+         <td colspan="4">
+           <div><span style="FONT-SIZE: 14pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><span style="FONT-SIZE: 14pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><strong>วารสารผู้ตรวจการแผ่นดิน ปีที่ 16 ฉบับที่ 2 กรกฎาคม - ธันวาคม 2566</strong></span></span></div>
+           <div><span style="FONT-SIZE: 14pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><span style="FONT-SIZE: 14pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><strong></strong></span></span></div></td>
+      </tr>
+       <tr>
+         <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">หน้าปก</span></div>
+           <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud001.mp3" target="_blank">Part 1</a> : <a href="download/audio/29_02_67/aud002.mp3" target="_blank">Part 2</a> : <a href="download/audio/29_02_67/aud003.mp3" target="_blank">Part 3</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+         <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">ปกใน</span></div>
+           <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud004.mp3" target="_blank">Part 4</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+         <td colspan="4">   
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">Contents Journal of Thai Ombudsman</span></div>
+           <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud005.mp3" target="_blank">Part 5</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+       <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">
+               <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">บทความพิเศษ : ผู้ตรวจการแผ่นดินกับการคุ้มครองสิทธิและเสรีภาพของประชาชน </span></div>
+               <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud006.mp3" target="_blank">Part 6</a> : <a href="download/audio/29_02_67/aud007.mp3" target="_blank">Part 7</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+         <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">บทความพิเศษ : เรื่องน่ารู้ผู้ตรวจการแผ่นดินรัฐสภาสวีเดน</span></div>
+           <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud008.mp3" target="_blank">Part 8</a> </span></div></blockquote></td>
+      </tr>
+       <tr>
+       <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">
+               <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">บทความวิชาการ : การพัฒนากฏหมาย องค์การมหาชนภายใต้หลักการเมืองบริหารกิจการบ้านเมืองที่ดี </span></div>
+               <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud009.mp3" target="_blank">Part 9</a> : <a href="download/audio/29_02_67/aud010.mp3" target="_blank">Part 10</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+      </tr>
+       <tr>
+       <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">
+               <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">บทความวิชาการ : รัฐที่นำการพัฒนาในโลกแห่งความเปลี่ยนแปลง และการพัฒนาเศรษฐกิจหมุนเวียนในเยอรมนี </span></div>
+               <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud011.mp3" target="_blank">Part 11</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+         <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">
+               <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">บทความวิชาการ : ภาษีการรับมรดกกับการลดความเหลื่อมล้ำทางเศรษฐกิจ </span></div>
+               <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud012.mp3" target="_blank">Part 12</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+       <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">
+               <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">บทความวิชาการ : บทบาทขององกรอิสระตามรัฐธรรมนูญ ในการรักษาวินัยทางการคลัง</span></div>
+               <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud013.mp3" target="_blank">Part 13</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+       <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">
+               <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">บทความวิชาการ : หลักประกันการจ่ายค่าชดเชย ข้อพิจารณาเพื่อแก้ปัญหาการเลิกจ้างโดยไม่จ่ายค่าชดเชย</span></div>
+               <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud014.mp3" target="_blank">Part 14</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+       <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">
+               <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">บทความวิจัย : การจัดการตนเองของชุมชน ศึกษากรณีป่าชุมชนในพื้นที่จ.กาญจนบุรี</span></div>
+               <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud016.mp3" target="_blank">Part 15</a> : <a href="download/audio/29_02_67/aud016.mp3" target="_blank">Part 16</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+       <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">
+               <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">คอลัมน์ประจำ : ท่านทราบหรือไม่ ผู้ตรวจการแผ่นดินย้ำ การแก้ไขปัญหาการถือครองที่ดินอุทยานแห่งชาติทับลาน ยึดถือความถูกตามต้องความเป็นจริงเพื่อประชาชน</span></div>
+               <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud017.mp3" target="_blank">Part 17</a> : <a href="download/audio/29_02_67/aud018.mp3" target="_blank">Part 18</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+         <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">เล่าเรื่องด้วยภาพ</span></div>
+           <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud019.mp3" target="_blank">Part 19</a></span></div></blockquote></td>
+      </tr>
+       <tr>
+         <td colspan="4">
+           <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff">ปกหลัง</span></div>
+           <blockquote style="MARGIN-RIGHT: 0px" dir="ltr">
+             <div><span style="FONT-SIZE: 12pt; FONT-FAMILY: Tahoma; COLOR: #ffffff"><a href="download/audio/29_02_67/aud020.mp3" target="_blank">Part 20</a> : <a href="download/audio/29_02_67/aud021.mp3" target="_blank">Part 21</a></span></div></blockquote></td>
+      </tr>
+    </tbody>
+  </table>
+</body>
